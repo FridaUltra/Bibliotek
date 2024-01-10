0c613c926aeaf1e48e25c5cefe4268c122ba9810
FridaUltra <5carlsonfrida@gmail.com>
2024-01-10 15:53:11 +0100

    Add unitTest

--- Library.Tests/UnitTest1.cs
+++ Library.Tests/UnitTest1.cs
@@ -1,6 +1,6 @@
 namespace Library.Tests;
 
-public class UnitTest1
+public class LibraryTests
 {
     [Fact]
     public void AddBook_book_ShouldAddBookToList()
@@ -11,10 +11,36 @@ public class UnitTest1
 
         // Act
         ca.AddBook(book);
-        List<Book> booksAddedToLibrary = ca.GetAllBooks();
+        //  List<Book> booksAddedToLibrary = ca.GetAllBooks();
 
         // Assert
 
-        Assert.True(booksAddedToLibrary.Count() == 1);
+        Assert.True(ca.GetAllBooks().Count() == 1);
     }
+
+    [Fact]
+    public void SearchForBook_searchString_ShouldReturnFoundBooks()
+    {
+        // Arrange
+        Catalogue ca = new Catalogue();
+
+        ca.AddBook(new Book("Saras äventyr", "Kristina Svensson", "2014"));
+        ca.AddBook(new Book("Harry Potter - De vises sten", "J.K Rowling", "2012"));
+        ca.AddBook(new Book("Harry Potter - Flammande bägaren", "J.K Rowling", "2015"));
+        ca.AddBook(new Book("Bamse", "Tomas Svensson", "2012"));
+
+        int expected = 2;
+
+        // Act
+        List<Book> foundBooks = ca.SearchForBook("Harry");
+        int actual = foundBooks.Count();
+
+
+        // Assert
+        Assert.Equal(expected, actual);
+
+
+    }
+
+
 }
